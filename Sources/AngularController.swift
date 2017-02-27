e369759f67fe4e3e4ea54aba086776a23f934285
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2017-02-27 13:19:17 +0100

    Added angular2 routes to server-side-render

--- Sources/AngularController.swift
+++ Sources/AngularController.swift
@@ -6,6 +6,7 @@
 //
 //
 
+import Foundation
 import PerfectHTTP
 
 public class AngularController {
@@ -13,19 +14,28 @@ public class AngularController {
     public func getRoutes() -> Routes {
         var routes = Routes()
         
-        routes.add(method: .get,  uri: "/home", handler: angularHandlerGET)
-        routes.add(method: .get,  uri: "/login", handler: angularHandlerGET)
-        routes.add(method: .get,  uri: "/account", handler: angularHandlerGET)
-        routes.add(method: .get,  uri: "/brand", handler: angularHandlerGET)
+        routes.add(method: .get,  uri: "/home",     handler: angularHandlerGET)
+        routes.add(method: .get,  uri: "/login",    handler: angularHandlerGET)
+        routes.add(method: .get,  uri: "/account",  handler: angularHandlerGET)
+        routes.add(method: .get,  uri: "/brand",    handler: angularHandlerGET)
         routes.add(method: .get,  uri: "/category", handler: angularHandlerGET)
-        routes.add(method: .get,  uri: "/attribute", handler: angularHandlerGET)
-        routes.add(method: .get,  uri: "/product", handler: angularHandlerGET)
+        routes.add(method: .get,  uri: "/attribute",handler: angularHandlerGET)
+        routes.add(method: .get,  uri: "/product",  handler: angularHandlerGET)
         
         return routes
     }
 
     func angularHandlerGET(request: HTTPRequest, _ response: HTTPResponse) {
-        //response.setHeader(.contentType, value: "application/json")
-        request.path = "/"
+        response.setHeader(.contentType, value: "text/html")
+        
+        let data = FileManager.default.contents(atPath: "./wwwroot/index.html")
+        
+        guard let content = String(data: data!, encoding: .utf8) else {
+            response.completed(status: HTTPResponseStatus.notFound)
+            return
+        }
+
+        response.appendBody(string: content)
+        response.completed()
     }
 }
