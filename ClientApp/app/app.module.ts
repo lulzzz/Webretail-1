48897976244a9752189ca96c0720696b02808d89
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2017-03-27 09:51:47 +0200

    Upgraded to angular 4

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 
 import { SharedModule } from './modules/shared/shared.module';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         AppRoutes,
         SharedModule.forRoot(),
         HomeModule
