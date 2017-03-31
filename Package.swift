e3179883e2bff639f66bec9db7e345cbbd2ad03b
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2017-04-01 00:58:44 +0200

    Refactoring

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-03-28 17:02:56 +0000
+// Date: 2017-03-31 22:53:18 +0000
 import PackageDescription
 let package = Package(
     name: "Webretail",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/iamjono/SwiftString.git", Version(1,0,13)),
-        .Package(url: "https://github.com/iamjono/SwiftRandom.git", Version(0,2,4)),
-        .Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", Version(1,0,9)),
+        .Package(url: "https://github.com/iamjono/SwiftString.git", Version(1,1,0)),
+        .Package(url: "https://github.com/iamjono/SwiftRandom.git", Version(0,2,5)),
+        .Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", Version(1,0,10)),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", Version(2,1,8)),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", Version(2,1,16)),
         .Package(url: "https://github.com/stormpath/Turnstile.git", Version(1,0,6)),
