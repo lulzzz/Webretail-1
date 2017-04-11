a16c6b04aff7856a2f894a12475cc80110444d57
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2017-04-11 23:57:00 +0200

    Add company setup

---
+++ Sources/Company.swift
@@ -0,0 +1,9 @@
+//
+//  Company.swift
+//  Webretail
+//
+//  Created by Gerardo Grisolini on 11/04/17.
+//
+//
+
+import Foundation
---
+++ Sources/CompanyController.swift
@@ -0,0 +1,9 @@
+//
+//  CompanyController.swift
+//  Webretail
+//
+//  Created by Gerardo Grisolini on 11/04/17.
+//
+//
+
+import Foundation
