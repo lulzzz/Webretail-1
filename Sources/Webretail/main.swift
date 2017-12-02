2a1ecf18eae33d93bed89919ef6529b6b218821e
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2017-12-02 02:23:18 +0100

    Refactorig

--- Sources/Webretail/main.swift
+++ Sources/Webretail/main.swift
@@ -38,14 +38,14 @@ SessionConfig.CORS.maxAge = 3000
 LogFile.location = "./StORMlog.txt"
 
 // Certs
-let cert = (sslCert: "cert.pem", sslKey: "key.pem")
+//let cert = (sslCert: "cert.pem", sslKey: "key.pem")
 
 // Create HTTP server.
 let server = HTTPServer()
 server.serverPort = 8181
 server.documentRoot = "./webroot"
-server.ssl = cert
-server.alpnSupport = [.http11, .http2]
+//server.ssl = cert
+//server.alpnSupport = [.http11, .http2]
 
 // Register dependency injection
 addIoC()
@@ -74,12 +74,12 @@ let angularRoutes = [
 // Launch the Web server.
 Threading.dispatch {
     _ = try? HTTPServer.launch(
-        .secureServer(
-            TLSConfiguration(certPath: cert.sslCert, keyPath: cert.sslKey, alpnSupport: server.alpnSupport),
-            name: "",
-            port: 8282,
-            routes: angularRoutes
-        ),
+//        .secureServer(
+//            TLSConfiguration(certPath: cert.sslCert, keyPath: cert.sslKey, alpnSupport: server.alpnSupport),
+//            name: "",
+//            port: 8282,
+//            routes: angularRoutes
+//        ),
         .server(
             name: "",
             port: 8080,
