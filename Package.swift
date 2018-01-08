8852bfe038f86f6c2f50bba83c33563a7a4ed5c0
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2018-01-08 22:57:45 +0100

    Update packages

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:4.0
 // Generated automatically by Perfect Assistant 2
-// Date: 2017-12-25 20:27:53 +0000
+// Date: 2018-01-08 21:24:16 +0000
 import PackageDescription
 
 let package = Package(
@@ -10,9 +10,9 @@ let package = Package(
 		.package(url: "https://github.com/iamjono/SwiftRandom.git", .exact("0.2.5")),
 		.package(url: "https://github.com/stormpath/Turnstile.git", .exact("1.0.6")),
 		.package(url: "https://github.com/SwiftORM/Postgres-StORM.git", .exact("3.0.1")),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", "3.0.1"..<"4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", .exact("3.0.3")),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .exact("3.0.3")),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", .exact("3.0.1")),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", .exact("3.1.1")),
 		.package(url: "https://github.com/twostraws/SwiftGD.git", .exact("2.0.1"))
 	],
 	targets: [
