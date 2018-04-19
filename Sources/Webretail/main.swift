c6e97e8eb9acefb041c97dc33c40a1d4efd03d86
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2018-04-20 01:51:37 +0200

    Deploy to heroku

--- Sources/Webretail/main.swift
+++ Sources/Webretail/main.swift
@@ -38,16 +38,16 @@ SessionConfig.CORS.maxAge = 3000
 LogFile.location = "./StORMlog.txt"
 
 // Certs
-let cert = (sslCert: "certificate.crt", sslKey: "private.pem")
-let alpn: [HTTPServer.ALPNSupport] = [.http11, .http2]
+//let cert = (sslCert: "certificate.crt", sslKey: "private.pem")
+//let alpn: [HTTPServer.ALPNSupport] = [.http11, .http2]
 
 // Create HTTP server.
 let server = HTTPServer()
 server.serverName = "www.webretail.cloud"
 server.serverPort = 8181
 server.documentRoot = "./webroot"
-server.ssl = cert
-server.alpnSupport = alpn
+//server.ssl = cert
+//server.alpnSupport = alpn
 
 // Register dependency injection
 addIoC()
@@ -58,39 +58,38 @@ addFilters()
 // Register routes and handlers
 addRoutesAndHandlers()
 
-// Angular routes
-let angularRoutes = [
-    Route(method: .get, uri: "/**", handler: try! HTTPHandler.staticFiles(data: ["documentRoot": "./WebUI/dist"])),
-    Route(method: .get, uri: "/home", handler: HTTPHandler.angularHandler(webapi: false)),
-    Route(method: .get, uri: "/account", handler: HTTPHandler.angularHandler(webapi: false)),
-    Route(method: .get, uri: "/login", handler: HTTPHandler.angularHandler(webapi: false)),
-    Route(method: .get, uri: "/register", handler: HTTPHandler.angularHandler(webapi: false)),
-    Route(method: .get, uri: "/products/{id}/{name}", handler: HTTPHandler.angularHandler(webapi: false)),
-    Route(method: .get, uri: "/product/{id}", handler: HTTPHandler.angularHandler(webapi: false)),
-    Route(method: .get, uri: "/basket", handler: HTTPHandler.angularHandler(webapi: false)),
-    Route(method: .get, uri: "/basket/{barcode}", handler: HTTPHandler.angularHandler(webapi: false)),
-    Route(method: .get, uri: "/checkout", handler: HTTPHandler.angularHandler(webapi: false)),
-    Route(method: .get, uri: "/orders", handler: HTTPHandler.angularHandler(webapi: false)),
-    Route(method: .get, uri: "/document/{id}", handler: HTTPHandler.angularHandler(webapi: false))
-]
+//// Angular routes
+//let angularRoutes = [
+//    Route(method: .get, uri: "/**", handler: try! HTTPHandler.staticFiles(data: ["documentRoot": "./WebUI/dist"])),
+//    Route(method: .get, uri: "/home", handler: HTTPHandler.angularHandler(webapi: false)),
+//    Route(method: .get, uri: "/account", handler: HTTPHandler.angularHandler(webapi: false)),
+//    Route(method: .get, uri: "/login", handler: HTTPHandler.angularHandler(webapi: false)),
+//    Route(method: .get, uri: "/register", handler: HTTPHandler.angularHandler(webapi: false)),
+//    Route(method: .get, uri: "/products/{id}/{name}", handler: HTTPHandler.angularHandler(webapi: false)),
+//    Route(method: .get, uri: "/product/{id}", handler: HTTPHandler.angularHandler(webapi: false)),
+//    Route(method: .get, uri: "/basket", handler: HTTPHandler.angularHandler(webapi: false)),
+//    Route(method: .get, uri: "/basket/{barcode}", handler: HTTPHandler.angularHandler(webapi: false)),
+//    Route(method: .get, uri: "/checkout", handler: HTTPHandler.angularHandler(webapi: false)),
+//    Route(method: .get, uri: "/orders", handler: HTTPHandler.angularHandler(webapi: false)),
+//    Route(method: .get, uri: "/document/{id}", handler: HTTPHandler.angularHandler(webapi: false))
+//]
 
-// Launch the Web server.
-Threading.dispatch {
-    _ = try? HTTPServer.launch(
-        .secureServer(
-            TLSConfiguration(certPath: cert.sslCert, keyPath: cert.sslKey, alpnSupport: alpn),
-            name: "www.webretail.cloud",
-            port: 8080,
-            routes: angularRoutes
-        )
-//        .server(
-//            name: "",
+//// Launch the Web server.
+//Threading.dispatch {
+//    _ = try? HTTPServer.launch(
+//        .secureServer(
+//            TLSConfiguration(certPath: cert.sslCert, keyPath: cert.sslKey, alpnSupport: alpn),
+//            name: "www.webretail.cloud",
 //            port: 8080,
 //            routes: angularRoutes
 //        )
-    )
-}
-
+////        .server(
+////            name: "",
+////            port: 8080,
+////            routes: angularRoutes
+////        )
+//    )
+//}
 
 do {
     // Setup database
