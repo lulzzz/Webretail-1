118fd39c562c0385f60a4f135fe746fc8b17dd86
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2018-04-21 00:06:58 +0200

    Fix heroku scripts

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV DEBIAN_FRONTEND noninteractive
 EXPOSE 8181
 
 RUN apt-get -y update && apt-get install -y libssl-dev libcurl4-openssl-dev libpq-dev uuid-dev libxml2-dev pkg-config wget
-RUN apt-get install build-essential chrpath libssl-dev libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 -y
+RUN apt-get install build-essential chrpath libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 -y
 RUN wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 RUN tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
 RUN ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
--- Package.swift
+++ Package.swift
@@ -7,16 +7,27 @@ let package = Package(
 	name: "Webretail",
 	dependencies: [
 		.package(url: "https://github.com/iamjono/SwiftString.git", .exact("2.0.4")),
-		.package(url: "https://github.com/iamjono/SwiftRandom.git", .exact("0.2.5")),
+		.package(url: "https://github.com/iamjono/SwiftRandom.git", .exact("1.0.0")),
 		.package(url: "https://github.com/stormpath/Turnstile.git", .exact("1.0.6")),
-		.package(url: "https://github.com/SwiftORM/Postgres-StORM.git", .exact("3.0.1")),
-		//.package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", .exact("3.0.3")),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .exact("3.0.3")),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", .exact("3.1.1")),
-		.package(url: "https://github.com/twostraws/SwiftGD.git", .exact("2.0.1")),
-        	.package(url: "https://github.com/gerardogrisolini/mwsWebretail.git", .exact("1.0.6"))
+		.package(url: "https://github.com/SwiftORM/Postgres-StORM.git", .exact("3.1.0")),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .exact("3.0.14")),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", .exact("3.2.1")),
+		.package(url: "https://github.com/twostraws/SwiftGD.git", .exact("2.1.1")),
+        .package(url: "https://github.com/gerardogrisolini/mwsWebretail.git", .exact("1.0.6"))
 	],
 	targets: [
-		.target(name: "Webretail", dependencies: ["SwiftString", "SwiftRandom", "PerfectHTTPServer", "PostgresStORM", "Turnstile", "PerfectSMTP", "SwiftGD", "mwsWebretail"])
+		.target(
+            name: "Webretail",
+            dependencies: [
+                "SwiftString",
+                "SwiftRandom",
+                "PerfectHTTPServer",
+                "PostgresStORM",
+                "Turnstile",
+                "PerfectSMTP",
+                "SwiftGD",
+                "mwsWebretail"
+            ]
+        )
 	]
 )
